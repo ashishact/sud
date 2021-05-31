f306410f0ebab106cf2d1a0a7b16ff52cc612b34
Ashish Tandi <ashishc@adaptideations.com>
2021-05-31 21:09:14 +0530

    Improvements on bulk data and time/interval set get

---
+++ later.js
@@ -0,0 +1,19 @@
+// navigator.usb.getDevices().then(devices => {
+    //     devices.forEach(device => {
+    //         console.log(device.productName);      // "Arduino Micro"
+    //         console.log(device.manufacturerName); // "Arduino LLC"
+    //     });
+    // })
+    // // await navigator.usb.
+    // vid_04d8&pid_0009#a1b2c3
+    // return;
+
+    /*
+    device = await navigator.usb.requestDevice({
+        filters: []
+    });
+
+    await device.open();
+    await device.selectConfiguration(1);
+    await device.claimInterface(0);
+    */
